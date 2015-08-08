1b6d3111e130c4be14af62cabcdeb914edaf5df9
Eddien <nguyenvolinh@gmail.com>
2015-08-09 01:36:50 +1000

    routing

--- microscope/server/fixtures.js
+++ microscope/server/fixtures.js
@@ -1,14 +1,17 @@
 if (Posts.find().count() === 0) {
 	Posts.insert({
 		title: "Introducing Telescope",
+		author: 'Eddie',
 		url: 'http://sachagreif.com'
 	});
 	Posts.insert({
 		title: "Meteor",
+		author: 'Tom',
 		url: 'http://meteor.com'
 	});
 	Posts.insert({
 		title: "The Meteor Book",
+		author: 'Eddie',
 		url: 'http://themeteorbook.com'
 	});
 }
